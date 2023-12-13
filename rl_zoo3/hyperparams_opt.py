1511a2c7980d581790a3f589ebdcd55cb66ae8b8
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2023-12-13 10:56:04 +0000

    updated hyperparams

--- rl_zoo3/hyperparams_opt.py
+++ rl_zoo3/hyperparams_opt.py
@@ -17,7 +17,7 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     """
     batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64, 128, 256, 512])
     n_steps = trial.suggest_categorical("n_steps", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
-    # gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1, log=True)
     # lr_schedule = "constant"
     # Uncomment to enable learning rate schedule
